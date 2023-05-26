8093718af5f63ef2f23f2dca9da7a45814cbec90
EmaCaputo <e.caputoblason@gmail.com>
2023-05-26 14:21:34 -0300

    login

--- app/src/main/java/ort/edu/ar/TP3/primerParcial/Grupo4/ui/login/LoginActivity.kt
+++ app/src/main/java/ort/edu/ar/TP3/primerParcial/Grupo4/ui/login/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import ort.edu.ar.tp3.primerparcial.grupo4.R
 import ort.edu.ar.tp3.primerparcial.grupo4.ui.main.MainActivity
@@ -14,12 +15,11 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // TODO: Implementar "Pseudo login" para mandar a la home
-        Handler(Looper.getMainLooper()).postDelayed({
+        var btnLogin = findViewById<Button>(R.id.btn_ingresar)
+
+        btnLogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            finish()
-        }, 1000)
+        }
     }
-
 }
