dd638d42d34865c31d805b93462f370206230b8e
Dardologo <dardo_015@hotmail.com>
2023-05-26 21:56:10 -0300

    WIP Setting Fragment

---
+++ app/src/main/java/ort/edu/ar/TP3/primerParcial/Grupo4/ui/settings/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package ort.edu.ar.TP3.primerParcial.Grupo4.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ort.edu.ar.tp3.primerparcial.grupo4.R
+
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+
+    }
